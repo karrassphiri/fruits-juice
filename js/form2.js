3cc1cd4344e7066c5bf19cc2f86563e27b69ab82
Karrass Phiri <phirikarrass@gmail.com>
2023-03-29 05:42:33 +0200

    customer page

---
+++ js/form2.js
@@ -0,0 +1,45 @@
+
+function checkSelected(fruit) {
+    return (fruit.name == document.getElementById('second-special').value) || (fruit.name == document.getElementById('first-special').value) || (fruit.name == document.getElementById('third-special').value);
+  }
+  
+  var btn = document.getElementById('submitButton');
+  btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById('modal-container').style.display = "flex";
+    var first = document.getElementById('first-special');
+    document.getElementById('First').innerHTML = first.value;
+    document.getElementById('Second').innerHTML = document.getElementById('second-special').value;
+    document.getElementById('Third').innerHTML = document.getElementById('third-special').value;
+  
+   
+    
+    fetch('https://karrassphiri.github.io/wdd230/bountiful/json/data.json')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+        const fruits = jsonObject['fruits'];
+        var filtered = fruits.filter(checkSelected);
+        var carbo = 0;
+        var fat = 0;
+        var protein = 0;
+        var sugar = 0;
+        var calories = 0;
+        for (const item of filtered) {
+          carbo += item['nutritions']['carbohydrates'];
+          fat += item['nutritions']['fat'];
+          protein += item['nutritions']['protein'];
+          sugar += item['nutritions']['sugar'];
+          calories += item['nutritions']['calories'];
+        }
+  
+        document.getElementById('summary').innerHTML = 'carbo: ' + carbo.toString() + '<br>' + 'fat: ' + fat.toString() + '<br>' + 'protein: ' + protein.toString() + '<br>' + 'sugar: ' + sugar.toString() + '<br>' + 'calories: ' + calories.toString() + '<br>';
+  
+      });
+  
+  })
+  
+  function showModal() {
+  
+  }
